f70042255db5d9fdf4876ee9828f85effa9810a0
rahulxyz <rahulguptaxyz15@gmail.com>
2021-01-03 01:53:23 +0530

    home:landing-section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import './Home.css';
+import React, { Component } from "react";
+import patternLeft from "../../assets/pattern-left.svg";
+import patternRight from "../../assets/pattern-right.svg";
+
+import "./Home.css";
 
 class Home extends Component {
+  state = {};
 
-    state = {}
-
-    render() {
-        return (
-            <section>
-                <div className="container">
-                    <div className="content">
-                        
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section>
+        <div className="container">
+          <div className="home-content">
+            <img src={patternLeft} alt="landing-img" />
+            <div className="description">
+              <h1>Dev Mentor</h1>
+              <p>Improve your developer skills by building real projects</p>
+              <div class="footer">
+                <button>Twit</button>
+                <button>Face</button>
+              </div>
+            </div>
+            <img src={patternRight} alt="landing-img" />
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Home;
